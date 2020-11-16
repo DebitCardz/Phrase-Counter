427d7f042e79ba0ca090d00ad35e4bb958a8fd85
Tech <debitcardzpaypal@gmail.com>
2020-11-16 12:43:37 -0500

    Updated user.ts

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,15 +1,8 @@
 import { Prop } from "@typegoose/typegoose";
-// import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
 
 export type Dictonary <T> = {
 	[key: string]: T
 }
-
-// Removed because this was moved to a variable instead of 
-// being stored in the database.
-// type Cooldown <T> = {
-	// [key: string]: T
-// }
 
 export class Gamer {
 	@Prop()
